3ab9e71ac044dcae909e4a50aff5a29ac7f2be59
Nut <35489639+Nutcasee@users.noreply.github.com>
2023-05-27 18:22:14 +0700

    parBlur, vertical...code from dude wontheone1, seems work

--- scalashop/src/main/scala/scalashop/common.scala
+++ scalashop/src/main/scala/scalashop/common.scala
@@ -43,31 +43,28 @@ def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA =
 
   // TODO implement using while loops
   // while x >= 0 do x = f(x)
-  var eachX = x - radius
-  var eachY = y - radius
-  var srcWidth = src.width
-  var srcHeight = src.height
-  var sumR = 0
-  var sumG = 0
-  var sumB = 0
-  var sumA = 0
-  var count = 0
-  
-  /** ah, i need 'support' from net for:
-    src(eachX, eachY)
-  */
-  while eachY <= min((y + radius), srcHeight) do
-    if clamp(eachY, -1, srcHeight + 1) > -1 then
-      while eachX <= min((x + radius), srcWidth) do
-        if clamp(eachX, -1, srcWidth + 1) > -1 then
-          sumR = sumR + red(src(eachX, eachY))  
-          sumG = sumG + green(src(eachX, eachY))
-          sumB = sumB + blue(src(eachX, eachY))
-          sumA = sumA + alpha(src(eachX, eachY))
-          count = count + 1
-        eachX = 1
-    eachY = eachY + 1
-  rgba(sumR / count, sumG / count, sumB / count, sumA / count)
+  var r = 0
+  var g = 0
+  var b = 0
+  var a = 0
+
+  var cnt = 0
+
+  var xi = clamp(x-radius, 0, src.width-1)
+  while(xi<=clamp(x+radius, 0, src.width-1)) {
+    var yi = clamp(y-radius, 0, src.height-1)
+    while(yi<=clamp(y+radius, 0, src.height-1)) {
+      r = r + red(src(xi, yi))
+      g = g + green(src(xi, yi))
+      b = b + blue(src(xi, yi))
+      a = a + alpha(src(xi, yi))
+      cnt = cnt + 1
+      yi = yi + 1
+    }
+    xi = xi + 1
+  }
+
+  rgba(r/cnt, g/cnt, b/cnt, a/cnt)
 
 val forkJoinPool = ForkJoinPool()
 
